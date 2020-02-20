9c4b4be4e2c77390cd435ecdbddbe6d8ac1b52f6
Karran8458 <kgowda@eckerd.edu>
2020-02-20 09:36:30 -0500

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+     public static void main(String []args){
+        String name = "Karran";
+        int years = 25;
+        String city = "Miami";
+        String state = "Florida";
+        System.out.println("My name is" + " " + name);
+        System.out.println("I am" + " " + years + " years old");
+        System.out.println("My hometown is" + " " + city + ", " + state);
+     }
+}
